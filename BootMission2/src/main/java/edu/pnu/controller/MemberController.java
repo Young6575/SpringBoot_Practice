31891ee6867c124942603083673f8da2bb21346b
Young6575 <wnsdud11510@gmail.com>
2025-07-25 18:01:09 +0900

    BootMission2 완성

--- BootMission2/src/main/java/edu/pnu/controller/MemberController.java
+++ BootMission2/src/main/java/edu/pnu/controller/MemberController.java
@@ -1,15 +1,62 @@
 package edu.pnu.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.pnu.domain.MemberDTO;
 import edu.pnu.service.MemberService;
 
 
 @RestController
+@RequestMapping("/api")
 public class MemberController {
+	private MemberService service = new MemberService(); 
+
 	
-	public MemberController() {
-		MemberService service = new MemberService();
+	@GetMapping("/member")
+	public List<MemberDTO> getAllMember() {
+		return service.getAllMember();
 	}
 	
+	@GetMapping("/member/{id}")   // 검색(Read – select One)
+	public MemberDTO getMemberById(@PathVariable Integer id) {
+		return service.getMemberById(id);
+	}
+	
+	@PostMapping("/member")
+	public MemberDTO postMember(@RequestBody MemberDTO memberDTO) {
+		return service.postMember(memberDTO); 
+	}
+	
+	@PutMapping("/member/{id}")
+	public MemberDTO putMember(@PathVariable Integer id,
+							   @RequestBody MemberDTO memberDTO) {
+		
+		return service.putMember(id, memberDTO);			
+	}
+	
+	@PatchMapping("/member/{id}") // 수정(Update – 일부 정보 수정)
+	public MemberDTO patchMember(@PathVariable Integer id,
+								 @RequestBody MemberDTO memberDTO) {
+		return service.patchMember(id, memberDTO);							
+	}
+
+	
+	
+	
+	@DeleteMapping("/member/{id}") // 삭제(Delete - delete)
+	public void deleteMember(@PathVariable Integer id) {
+		service.deleteMember(id);
+	}
+
+
 }
--- BootMission2/src/main/java/edu/pnu/service/MemberService.java
+++ BootMission2/src/main/java/edu/pnu/service/MemberService.java
@@ -11,13 +11,74 @@ public class MemberService {
 	List<MemberDTO> list = new ArrayList<>(); 
 	
 	public MemberService() {
-		for (int i = 1; i <= 2 ; i++ ) {
+		for (int i = 1; i <= 10 ; i++ ) {
 			list.add(MemberDTO.builder().id(i).name("name"+i).pass("pass" + i)
 										.regidate(new Date()).build());
 		}
 	}
 	
 	
+	public List<MemberDTO> getAllMember() {
+		return list;
+	}
+	
+	public MemberDTO getMemberById(Integer id) {
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i).getId().equals(id)) {
+				return list.get(i);
+			}
+		}
+		return null;
+	}
+	public MemberDTO postMember(MemberDTO memberDTO) {
+		
+		list.add(memberDTO);
+		return memberDTO;
+		
+	}
+	
+	public MemberDTO putMember(Integer id, MemberDTO memberDTO) {
+		
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				list.remove(i);
+				list.add(i, memberDTO);
+				return memberDTO;
+			}
+		}
+		return null;							
+	}
+	
+	public MemberDTO patchMember(Integer id, MemberDTO memberDTO) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				MemberDTO orgin = list.get(i);
+				if (memberDTO.getPass() != null) {
+					orgin.setPass(memberDTO.getPass());
+				}
+				if (memberDTO.getName() != null) {
+					orgin.setName(memberDTO.getName());
+				}
+				if (memberDTO.getRegidate() != null) {
+					orgin.setRegidate(memberDTO.getRegidate());
+				}
+				list.set(i, orgin);
+				return orgin;
+			}
+		}
+		return null;							
+	}
+
+	
+	
+	
+	public void deleteMember(Integer id) {
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i).getId().equals(id)) {
+				list.remove(i);
+			}
+		}
+	}
 	
 	
 	
